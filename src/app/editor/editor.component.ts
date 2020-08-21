a57bd0b0f5f0eac22b9a59fa143db5c2c8d86fd1
newmediapilot <marcin@newmediapilot.com>
2020-08-21 01:05:01 -0400

    build the forms with validation

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, RequiredValidator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'nmp-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent {
 
-  constructor() { }
+  reminderTypeGroup: FormGroup;
+  reminderTextGroup: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.fb = new FormBuilder();
+
+    // reminderType form
+    this.reminderTypeGroup = fb.group({
+      reminderTypeSelect: fb.control('', [
+        Validators.required
+      ])
+    });
+
+    // reminderText form
+    this.reminderTextGroup = fb.group({
+      reminderText: fb.control('', [
+        Validators.required
+      ])
+    });
+
+  }
+
+  setReminder() {
+    var output: object = Object.assign(
+      this.reminderTypeGroup.getRawValue(),
+      this.reminderTextGroup.getRawValue()
+    );
+    // TODO: save to service
   }
 
 }
