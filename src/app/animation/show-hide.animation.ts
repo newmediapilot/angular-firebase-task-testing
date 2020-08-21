f7da97792730f9345906183eaa5cec0d96461f22
newmediapilot <marcin@newmediapilot.com>
2020-08-20 20:36:44 -0400

    TODO added

--- src/app/animation/show-hide.animation.ts
+++ src/app/animation/show-hide.animation.ts
@@ -1,5 +1,6 @@
 import {animate, style, transition, trigger} from '@angular/animations';
 
+//TODO: make this work with router enter/leave
 export const showInit = trigger('showInit', [
   transition(':enter', [
     style({opacity: 0}),
